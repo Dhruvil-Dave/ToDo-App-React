6b623ceacaab959276dd2fc3ff73f9604c0b9d6d
Dhruvil-Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-02-05 00:09:27 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Ap from './Ap';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Ap />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
